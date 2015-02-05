161071a318fa41f925f2303aa42d01aedb3cbda2
kirikiriyamama <kirikiriyamama@icloud.com>
2015-02-06 03:13:47 +0900

    Disable memoization in `Soror::EC2::Instance.search_by`

--- lib/soror/ec2/instance.rb
+++ lib/soror/ec2/instance.rb
@@ -4,12 +4,9 @@ module Soror
       extend Soror::EC2
 
       def self.search_by(tags)
-        warn %q{[DEPRECATION] `Soror::EC2::Instance.search_by` won't be memoized. If you want to memoize, use `Soror.start_memoizing`.}
-        AWS.memoize do
-          ec2.instances.
-            filter('instance-state-name', 'running').
-            select{ |i| tags.all?{ |k, v| i.tags[k.to_s] == v.to_s } }
-        end
+        ec2.instances.
+          filter('instance-state-name', 'running').
+          select{ |i| tags.all?{ |k, v| i.tags[k.to_s] == v.to_s } }
       end
     end
   end
