e62e6c984b62a5c5899531f19280f42b1a0f8a04
kirikiriyamama <kirikiriyamama@gmail.com>
2017-09-05 18:21:55 +0900

    Improve implementation

--- lib/soror.rb
+++ lib/soror.rb
@@ -1,12 +1,34 @@
 require 'aws-sdk-ec2'
 require 'forwardable'
-require 'soror/ec2'
 require 'soror/version'
 
-module Soror
+class Soror
   class << self
     extend Forwardable
+
     def_delegators Aws, :config, :config=
-    def_delegators Soror::EC2::Instance, :search_by
+    def_delegators new, :search_by
+  end
+
+  def initialize
+    @ec2 = Aws::EC2::Client.new
+  end
+
+  def search_by(tags)
+    filters = [{ name: 'instance-state-name', values: ['running'] }]
+    tags.each do |k, v|
+      filters << { name: "tag:#{k}", values: [v] }
+    end
+
+    @ec2.describe_instances(filters: filters).reservations.map(&:instances).flatten
+  end
+
+  module EC2
+    class Instance
+      # Deprecated
+      def self.search_by(tags)
+        Soror.search_by(tags)
+      end
+    end
   end
 end
